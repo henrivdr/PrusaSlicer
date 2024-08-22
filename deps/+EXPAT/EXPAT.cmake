1c9f363817ee4c7e47fd1ffa38dec51067243aed
Henri van der Riet <henrivdr@gmail.com>
2024-08-22 16:36:52 +0200

    bump libexpat to 2.6.2

--- deps/+EXPAT/EXPAT.cmake
+++ deps/+EXPAT/EXPAT.cmake
@@ -1,6 +1,8 @@
 add_cmake_project(EXPAT
-  URL https://github.com/libexpat/libexpat/archive/refs/tags/R_2_4_3.zip
-  URL_HASH SHA256=8851e199d763dc785277d6d414ed3e70ff683915158b51b8d8781df0e3af950a
+  # URL https://github.com/libexpat/libexpat/archive/refs/tags/R_2_4_3.zip
+  # URL_HASH SHA256=8851e199d763dc785277d6d414ed3e70ff683915158b51b8d8781df0e3af950a
+  URL https://github.com/libexpat/libexpat/archive/refs/tags/R_2_6_2.zip
+  URL_HASH SHA256=9cddaf9abdac4cb3308c24fea13219a7879c0ad9a8e6797240b9cf4770b337b4
   SOURCE_SUBDIR expat
   CMAKE_ARGS
     -DEXPAT_BUILD_TOOLS:BOOL=OFF
